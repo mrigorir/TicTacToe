c8c7d55b0e08120cd468d81f85b31465480aaca1
Anvi Alex <epononanvialex@gmail.com>
2021-04-01 22:27:11 +0300

    Pull milestone3

--- lib/logic.rb
+++ lib/logic.rb
@@ -17,40 +17,23 @@ class TicTacToe
     false
   end
 
-  def arbiter(array, user_input , player_char)
+  def arbiter(array, user_input, player_char)
     @array = array
     @player_char = player_char
-    @array = [0,1,2,3,4,5,6,7,8]
-    @array2 = []
-    @array3 = []
-    @array4 = []
-    @array5 = []
-    @array2 << array[0..2]
-    @array3 << array[3..5]
-    @array4 << array[6..8]
-    array5.push(array2, array3, array4)
-    array6 = array5.flatten(1)
 
-    p "hello"
     @user_input = user_input.to_i
     @indexes_win = [[0, 4, 8], [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
     @i = 0
     while @i < @indexes_win.length
       if @indexes_win[@i].any?(@user_input)
-          @indexes_win[@i].each do |index|
-            p @array
-            if @indexes_win[@i].all?(@array.at(index)) #if board.select.each_with_index {|item, index| index == }
-              @player_char[0]
-              break
-            elsif @array.at(index).all?(@player_char[1])
-              @player_char[1]
-              break
-            else
-              return "TIE"
-              break
+        @indexes_win[@i].each do |index|
+          p array.each do |arr_element|
+            if array.find_index_of(arr_element) == index
+              p "he"
             end
           end
         end
+      end
       @i += 1
     end
   end
