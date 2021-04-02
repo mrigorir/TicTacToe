5f71c4cf11beacfe80f1aafb4ecb5f3bd51055cf
Marco <marcoparra311@gmail.com>
2021-04-01 21:18:36 -0400

    Finish 2 of 3 winning cases

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,7 +1,10 @@
 class TicTacToe
   attr_accessor :token, :player
-  attr_reader :board, :game, :player_char, :loc, :move
-  def initialize; end
+  attr_reader :board, :game, :player_char, :loc, :move, :winner
+  def initialize 
+    @indexes_win = [[0, 4, 8], [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
+    @winner = ""
+  end
 
   def check_input(array, player_char, player, loc, usedd, move)
     @player = player
@@ -19,60 +22,24 @@ class TicTacToe
 
   def arbiter(array, user_input , player_char)
     @array = array
-    @pos = []
     @player_char = player_char
     @user_input = user_input
-    @indexes_win = [[0, 4, 8], [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
     
-    if array.count(player_char) >= 3
-      @array.each.with_index do |el, i|
-        @pos << i if el == player_char 
-        @pos
+    @indexes_win.each do |sub_array|
+      sub_array.map.with_index do |el, i|
+        sub_array[i] = @player_char if el == @user_input.to_i
+        sub_array
       end
-      p @pos
-     @indexes_win.each do |sub_array|
-      if player_char == 'X'
-        p 'P1 wins' if sub_array.eql?(@pos)
-      elsif player_char == 'O'
-        p 'P2 wins' if sub_array.eql?(@pos)
-      else
-        p 'TIE'
-        return false
+      if sub_array.count('X') == 3
+        @winner = 'P1 wins.'
+        return 'win'
+      end
+      if sub_array.count('O') == 3
+        @winner = 'P2 wins.'
+        return 'win'
       end
     end
-  end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #@i = 0
-    #while @i < @indexes_win.length
-      #if @indexes_win[@i].any?(@user_input)
-         # @indexes_win[@i].each do |index|
-            #p @array
-            #if @indexes_win[@i].all?(@array.at(index))
-              #@player_char[0]
-              #break
-            #elsif @array.at(index).all?(@player_char[1])
-              #@player_char[1]
-              #break
-            #else
-              #return "TIE"
-              #Ebreak
-            #end
-          #end
-        #end
-      #@i += 1
-    #end
+    return ""   
   end
 
   private
