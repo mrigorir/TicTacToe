bd9f6bdfba63ef2526cac30069632584f444c1ef
Anvi Alex <epononanvialex@gmail.com>
2021-04-02 23:37:21 +0300

    Fix last linter errors

--- lib/logic.rb
+++ lib/logic.rb
@@ -3,6 +3,7 @@ class TicTacToe
   attr_reader :board, :game, :player_char, :loc, :move, :winner
   def initialize
     @indexes_win = [[1, 5, 9], [1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
+
     @winner = ''
     @county = 1
   end
@@ -23,11 +24,9 @@ class TicTacToe
 
   def pseudo_empty?(player)
     @player = player
-    if @player.empty? || @player.nil?
-      true
-    else
-      false
-    end
+    return true if @player.empty? || @player.nil?
+
+    false
   end
 
   def arbiter(array, user_input, player_char, playername)
@@ -35,7 +34,6 @@ class TicTacToe
     @player_char = player_char
     @user_input = user_input
     @playername = playername
-
     @indexes_win.each do |sub_array|
       sub_array.map.with_index do |el, i|
         sub_array[i] = @player_char if el == @user_input.to_i
@@ -49,13 +47,19 @@ class TicTacToe
         return 'win'
       end
     end
-    print "It's a TIE !" if @county == 9
+    if @county == 9
+      print "It's a TIE !\n"
+      return 'tie'
+    end
     @county += 1
+
+    return '' if @county <= 9
 
     ''
   end
 end
 
+private
 class Player
   attr_accessor :name, :token
 
